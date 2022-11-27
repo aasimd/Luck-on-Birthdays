409be4210ef0b520472eb94ab7f077e2733d896d
aasimd <dongarkar21@gmail.com>
2022-11-27 18:42:02 +0530

    script done

---
+++ main.js
@@ -0,0 +1,32 @@
+var birthDate = document.querySelector("#birth-date");
+var luckyNumber = document.querySelector("#lucky-number");
+var button = document.querySelector("#check-button");
+var results = document.querySelector("#results");
+
+function clickHandler(){
+    var dob = birthDate.value;
+    dob = dob.replaceAll("-","");
+    var birthDateSum = dbSum (dob);
+    if (birthDateSum&&luckyNumber.value){
+    calculateResults(birthDateSum);
+    }else{
+        results.innerText = "Please enter both the fields!! 😡"
+    }
+}
+
+function dbSum(dob){
+    let sum = 0;
+    for (let i = 0; i < dob.length; i++){
+        sum = sum + Number(dob.charAt(i));
+    } return sum
+}
+
+function calculateResults(birthDateSum){
+    if (birthDateSum%luckyNumber.value===0){
+        results.innerText = "Your Birthday is Lucky 🕺";
+    } else {
+        results.innerText = "Your Birthday is not Lucky 🙀, But you Do create your Own Luck 😁";
+    }
+}
+
+button.addEventListener("click", clickHandler)
